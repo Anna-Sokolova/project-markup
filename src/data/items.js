cf3f8a0a55b47f33935f518637cb8ead9527fcb9
Anna Sokolova <sokolovaskorik@gmail.com>
2020-11-29 13:45:27 +0200

    Adds section about

---
+++ src/data/items.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    name: "Ferrari",
+    picture: [
+      "https://cdn.motor1.com/images/mgl/0x6J3/s1/ferrari-sf90-stradale.jpg",
+      "https://motor.ru/thumb/2250x0/filters:quality(75):no_upscale()/imgs/2019/05/30/08/3389485/f13ce58b1fb8ec236f1eb4f3f9ae93716f29a810.jpeg",
+    ],
+    price: 100000,
+  },
+  {
+    name: "Lamborgini",
+    picture: [
+      "https://i2.wp.com/itc.ua/wp-content/uploads/2019/09/Lamborghini-Sian.jpg?fit=2000%2C1128&quality=100&strip=all&ssl=1",
+      "https://cdn.motor1.com/images/mgl/1x9W7/s1/le-10-lamborghini-piu-rare-e-costose-di-sempre.jpg",
+    ],
+    price: 150000,
+  },
+  {
+    name: "Bugatti",
+    picture: [
+      "https://img.drive.ru/i/0/5d75ea9fec05c4a22a000015.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/1/16/Bugatti_Divo%2C_GIMS_2019%2C_Le_Grand-Saconnex_%28GIMS0029%29.jpg",
+    ],
+    price: 200000,
+  },
+  {
+    name: "Lanos",
+    picture: [
+      "https://i.infocar.ua/i/12/154/700x350.jpg",
+      "https://cdn.segodnya.ua/i/image_650x434/media/image/5ca/68d/c09/5ca68dc09999e.jpg",
+    ],
+    price: 3000,
+  },
+];
--- src/js/about.js
+++ src/js/about.js
@@ -1,1 +1,45 @@
 import "../css/about.css";
+import myAvtoPark from "../data/items.js";
+// console.log(myAvtoPark);
+import aboutRefs from "./refs.js";
+const { aboutSection } = aboutRefs; //деструктуризация объекта
+// console.log(aboutSection);
+
+function cresteCarsList(array) {
+  //   console.log(array);
+  return array.map((car) => {
+    const { name, picture, price } = car;
+    // console.log(picture);
+    const imgName = document.createElement("h3");
+    imgName.textContent = name;
+    // console.log(name);
+
+    const imgWrapper = document.createElement("div");
+
+    const createPictures = picture.map((pic) => {
+      const img = document.createElement("img");
+      img.setAttribute("src", pic);
+      img.setAttribute("alt", name);
+      img.setAttribute("width", "300px");
+      //   console.log(img);
+      return img;
+    });
+    imgWrapper.append(...createPictures);
+    // console.log(imgWrapper);
+
+    const carPrice = document.createElement("p");
+    carPrice.textContent = price;
+    // console.log(price);
+    const item = document.createElement("li");
+    item.append(imgName, imgWrapper, carPrice);
+    return item;
+  });
+}
+const carsItems = cresteCarsList(myAvtoPark);
+console.log(carsItems);
+
+const carsList = document.createElement("ul");
+carsList.append(...carsItems);
+console.log(carsList);
+
+aboutSection.append(carsList);
